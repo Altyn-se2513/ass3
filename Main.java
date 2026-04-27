3c36906e6b14a757c35c1e82eff06dcfbaf3aeb2
Altyn-se2513 <250886@astanait.edu.kz>
2026-04-27 21:29:06 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Create an instance of Experiment class to run all tests
+        Experiment experiment = new Experiment();
+
+        // Start all experiments: sorting and searching with different array sizes
+        experiment.runAllExperiments();
+    }
+}
